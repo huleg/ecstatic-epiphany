8714ea4d8db6fa25ddbf5a37c7c4bf30880c5e17
Micah Elizabeth Scott <micah@scanlime.org>
2014-04-23 10:45:32 -0700

    Turn camera driver on

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,22 @@
-// XXX hacky test code
+/*
+ * Ecstatic Epiphany
+ *
+ * (c) 2014 Micah Elizabeth Scott
+ * http://creativecommons.org/licenses/by/3.0/
+ */
 
-#include <time.h>
-
-#include "lib/effect.h"
-#include "lib/effect_runner.h"
-#include "lib/effect_mixer.h"
-#include "lib/particle.h"
 #include "lib/camera.h"
-#include "lib/camera_framegrab.h"
-#include "lib/camera_sampler.h"
-#include "lib/prng.h"
 #include "narrator.h"
 
-static CameraFramegrab grab;
 static Narrator narrator;
-
 
 static void videoCallback(const Camera::VideoChunk &video, void *)
 {
-    if (grab.isGrabbing()) {
-        grab.process(video);
-    }
-    // narrator.vismem.process(video);
 }
 
 int main(int argc, char **argv)
 {
-    // Camera::start(videoCallback);
+    Camera::start(videoCallback);
 
     narrator.runner.setLayout("layouts/window6x12.json");
     if (!narrator.runner.parseArguments(argc, argv)) {
